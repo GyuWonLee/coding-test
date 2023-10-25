626832213f0896ecf9fa3d8ba9f5f38982da5c99
GYUWON <37896874+GyuWonLee@users.noreply.github.com>
2023-10-25 14:49:20 +0900

    Add files via upload

---
+++ 덧셈식 계산하기.java
@@ -0,0 +1,23 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("%d + %d = %d",a,b,a+b);
+    }
+}
