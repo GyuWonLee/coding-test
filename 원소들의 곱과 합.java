e929496dfdee7aa659a2fd9c9d40820ffc8da9c8
GYUWON <37896874+GyuWonLee@users.noreply.github.com>
2023-10-28 20:53:50 +0900

    Add files via upload

---
+++ 원소들의 곱과 합.java
@@ -0,0 +1,34 @@
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int sum = 0;
+        int mul = 1;
+        for(int i=0; i<num_list.length; i++){
+            sum += num_list[i];
+            mul *= num_list[i];
+        }
+        if(sum * sum > mul){
+            answer = 1;
+        }else{
+            answer = 0;
+        }
+        return answer;
+    }
+}
