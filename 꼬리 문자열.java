dee89f00d442f20fcb81e2bf7302b1560b673025
GYUWON <37896874+GyuWonLee@users.noreply.github.com>
2023-11-25 19:41:46 +0900

    Add files via upload

---
+++ 꼬리 문자열.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Solution {
+    public String solution(String[] str_list, String ex) {
+        StringBuilder tailString = new StringBuilder();
+
+        for (String str : str_list) {
+            if (!str.contains(ex)) {
+                tailString.append(str);
+            }
+        }
+
+        return tailString.toString();
+    }
+}
