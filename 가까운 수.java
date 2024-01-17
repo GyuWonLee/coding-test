ff9e945538e72f12e0fe3908cea14d378e649f51
GYUWON <37896874+GyuWonLee@users.noreply.github.com>
2024-01-17 20:46:07 +0900

    Add files via upload

---
+++ 가까운 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] array, int n) {
+
+        int closest = array[0];
+
+        for (int num : array) {
+            if (Math.abs(num - n) < Math.abs(closest - n) || 
+                (Math.abs(num - n) == Math.abs(closest - n) && num < closest)) {
+                closest = num;
+            }
+        }
+
+        return closest;
+    }
+}
